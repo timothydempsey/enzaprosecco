027e70b6009e788e0b1c7383f92b7488f552cd63
Timothy Dempsey <tim@dempseystudio.com>
2018-03-22 10:36:11 -0400

    align github

---
+++ about-enza-prosecco.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Enza Prosecco | About</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<!-- COMMENT -->
+<body>
+<div style="background-color:#bcbaca">
+	<div class="container">  
+		<nav id="primary_nav_wrap">
+			<?php include("includes/navig.php"); ?>
+		</nav>
+	</div>
+	</div>
+<div class="container">
+  <div id="page-content">
+    
+     	  <img src="images/About-Enza-Prosecco.png" alt="AboutEnza Prosecco"/>
+<div style="width:60%;position:relative;top:-300px">
+<h1>ABOUT ENZA</h1>
+	<h2>Our Story</h2>
+<p>Named after the picturesque river in Northern Italy, Enza is quintessentially Italian.</p>
+ <p>Founded in 1915 by Sisto Sacchetto, today, Filiberto Sacchetto and his son Paolo carry on the Cantina Sacchetto tradition of producing DOC wines in the Veneto Region of Northern Italy. The Sacchetto family crafts each bottle of Enza to capture the essence of DOC Prosecco. Combining a century of tradition with modern winemaking techniques, Enza captures the refreshing yet complex virtues of Prosecco sophisticated wine lovers have come to savor.</p>
+ <p>Enza lightens the moment with a sophisticated sparkle to make your everyday extraordinary!</p>
+  </div>
+</div>
+	</div>
+<footer>
+<?php include("includes/footer.php"); ?><!-- this is a comment -->
+</footer>
+</body>
+</html>
